0d5f6bb5172463a16c3479616cbd2a4dadc14998
igord <igord@igord-VirtualBox.(none)>
2014-04-24 01:11:37 +0200

    pyfile01 added

---
+++ pyfile01.py
@@ -0,0 +1,21 @@
+from pypack.Animal import Animal
+
+__author__ = 'igord'
+
+
+def main():
+    # print("Hello2")
+    # animal = Animal(45)
+    # animal.double_age()
+    # print(animal.age)
+
+    print("Start")
+
+    msg = "ana i mujica"
+    msg2 = msg.replace("a", "$")
+    print(msg)
+    print(msg2)
+
+    print("End")
+
+main()
