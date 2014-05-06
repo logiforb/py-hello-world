cf371174cbff52e1c3ffc2148404b4bdc9137dff
logiforb <logiforb@users.noreply.github.com>
2014-05-06 20:22:42 +0200

    WMSmvc01 first build

---
+++ Java/Jgit01/WMSmvc01/src/main/java/com/codetutr/controller/FormController.java
@@ -0,0 +1,16 @@
+
+package com.codetutr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FormController {
+	
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String hello() {
+		return "index";
+	}
+}
